9abbaf01c27f23b42e3518d705f09d60cf26c780
A <34955019+Abient@users.noreply.github.com>
2022-04-18 09:23:01 -0700

    Add files via upload

---
+++ scripts/deploy.js
@@ -0,0 +1,24 @@
+// scripts/deploy.js
+async function main() {
+
+	const [deployer] = await ethers.getSigners();
+
+	console.log(
+	"Deploying contracts with the account:",
+	deployer.address
+	);
+
+	console.log("Account balance:", (await deployer.getBalance()).toString());
+
+	const swapandstore = await ethers.getContractFactory("swapandstore");
+	const contract = await swapandstore.deploy();
+
+	console.log("Contract deployed at:", contract.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+	console.error(error);
+	process.exit(1);
+  });
